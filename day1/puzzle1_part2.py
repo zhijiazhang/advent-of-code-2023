f905b7e2c2e96387aae788dcac6d5078cb7b07db
Zhijia <zhijiaxzhang@gmail.com>
2023-12-01 13:18:21 -0500

    making day 1 code cleaner

--- day1/puzzle1_part2.py
+++ day1/puzzle1_part2.py
@@ -9,24 +9,20 @@ nums = {
 
 def calc(line: str, i: int, n: int) -> str:
     char = line[i]
-    if char.isnumeric():
-        return char
+    if char.isnumeric(): return char
+    if char not in nums: return None
     
-    if char in nums:
-        for digit, number, length in nums[char]:
-            if i + length - 1 < n:
-                if line[i:i+length] == number:
-                    return digit
-    return None         
+    for digit, number, length in nums[char]:
+        if i + length - 1 < n and line[i:i+length] == number:
+                return digit
+        
 
-def left_digit(line: str):
-    n = len(line)
+def left_digit(line: str, n: int) -> str:
     for i in range(n):
         res = calc(line, i, n)
         if res: return res
 
-def right_digit(line:str):
-    n = len(line)
+def right_digit(line:str, n: int) -> str:
     for i in range(len(line) - 1, - 1, -1):
         res = calc(line, i, n)
         if res: return res
@@ -37,7 +33,9 @@ def solve(filename: str) -> int:
 
     with open(filename, "r") as reader:
         for line in reader.readlines():
-            total += int(left_digit(line) + right_digit(line))
+            n = len(line)
+            total += int(left_digit(line, n) + right_digit(line, n))
                      
     return total
+
 
