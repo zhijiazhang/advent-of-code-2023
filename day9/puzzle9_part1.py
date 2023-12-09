3e57972bd50c27fc60aa9f78286a57765afe7168
Zhijia <zhijiaxzhang@gmail.com>
2023-12-09 02:01:19 -0500

    finished day 9

---
+++ day9/puzzle9_part1.py
@@ -0,0 +1,48 @@
+
+def extrapolate(arr):
+    return sum(arr[i][-1] for i in range(len(arr) - 2, - 1, -1))
+
+def next_row(arr):
+    next_row = []
+    zero = True
+    for i in range(1, len(arr)):
+        diff = arr[i] - arr[i - 1]
+        next_row.append(diff)
+        if diff != 0: zero = False
+    return (next_row, zero)
+
+
+def solve(filename:str) -> int:
+
+    arrays = []
+
+    with open(filename) as reader:
+        for line in reader.readlines():
+            arrays.append([int(i) for i in line.split()])
+
+    total = 0
+
+    for arr in arrays:
+        curr = [arr]
+        while True:
+            next_, zero = next_row(curr[-1])
+            curr.append(next_)
+            if zero : break
+
+        total += extrapolate(curr)
+
+    return total
+
+
+
+    
+
+    
+
+
+
+
+
+
+
+print(solve("/Users/zhijiazhang/Desktop/advent-of-code-2023/day9/puzzle9.txt"))
